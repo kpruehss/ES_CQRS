f452f68afeabdaa408c2a1b2158b18e174800ca9
Karsten Pruehss <Karsten@Pruehss.com>
2025-05-10 16:04:23 +1000

    add command router

---
+++ BeerSender.Domain/CommandRouter.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Input;
+using BeerSender.Domain.Boxes.Commands;
+
+namespace BeerSender.Domain;
+
+public class CommandRouter(IEventStore eventStore, IServiceProvider serviceProvider)
+{
+    public void HandleCommand(object command)
+    {
+        var commandType = command.GetType();
+        
+        var handlerType = typeof(CommandHandler<>).MakeGenericType(commandType);
+
+        var handler = serviceProvider.GetService(handlerType);
+        
+        var methodInfo = handlerType.GetMethod("Handle");
+        
+        methodInfo?.Invoke(handler, [command]);
+        
+        eventStore.SaveChanges();
+    }
+}
