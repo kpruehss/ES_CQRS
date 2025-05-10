c19da82dc735135a1d165b65a6c54a76ea3c1aa4
Karsten Pruehss <Karsten@Pruehss.com>
2025-05-10 16:03:58 +1000

    Build event stream

---
+++ BeerSender.Domain/EventStream.cs
@@ -0,0 +1,33 @@
+﻿namespace BeerSender.Domain;
+
+public class EventStream<TEntity>(IEventStore eventStore, Guid aggregateId)
+    where TEntity : AggregateRoot, new()
+{
+    private int _lastSequenceNumber;
+    public TEntity GetEntity()
+    {
+        var events = eventStore.GetEvents(aggregateId);
+
+        TEntity entity = new();
+        foreach (var @event in events)
+        {
+            entity.Apply((dynamic)@event.EventData);
+            _lastSequenceNumber = @event.SequenceNumber;
+        }
+
+        return entity;
+    }
+
+    public void Append(object @event)
+    {
+        _lastSequenceNumber++;
+
+        StoredEvent storedEvent = new(
+            aggregateId,
+            _lastSequenceNumber,
+            DateTime.UtcNow,
+            @event);
+
+        eventStore.AppendEvent(storedEvent);
+    }
+}
---
+++ BeerSender.Domain/IEventStore.cs
@@ -0,0 +1,8 @@
+﻿namespace BeerSender.Domain;
+
+public interface IEventStore
+{
+    IEnumerable<StoredEvent> GetEvents(Guid aggregateId);
+    void AppendEvent(StoredEvent @event);
+    void SaveChanges();
+}
